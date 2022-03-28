aa949a008bdf82ea8323cf4fbd3b1de2e1d37c27
heeyong222 <laru1072@gmail.com>
2022-03-28 22:02:08 +0900

    feat : Nav ing

--- src/main/java/com/hy/app/navs/NavsController.java
+++ src/main/java/com/hy/app/navs/NavsController.java
@@ -1,7 +1,18 @@
 package com.hy.app.navs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
+@RequestMapping(path="/navs")
 public class NavsController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NavsController.class);
+
+
+
 }
