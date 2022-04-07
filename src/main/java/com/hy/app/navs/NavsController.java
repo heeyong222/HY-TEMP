8fdfcb7c5aa1ddfe3d260551589af77dbe353694
heeyong222 <laru1072@gmail.com>
2022-04-07 22:29:29 +0900

    feat : Nav ing

--- src/main/java/com/hy/app/navs/NavsController.java
+++ src/main/java/com/hy/app/navs/NavsController.java
@@ -6,10 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -28,7 +25,7 @@ public class NavsController {
 
 
     @PostMapping("/find")
-    public ResponseEntity<ResultMessage> find(Map<String, Object> map){
+    public ResponseEntity<ResultMessage> find(@RequestBody Map<String, Object> map){
         LOGGER.debug(">>> API find - param : {}", map.toString());
         ResultMessage rm = null;
 
